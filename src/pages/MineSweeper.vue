ee9f70810b711549af6f83963d75bada8ece0cfe
2389451262@qq.com <2389451262@qq.com>
2022-05-20 21:38:18 +0800

    chore: make page adapt to phone ui

--- src/pages/MineSweeper.vue
+++ src/pages/MineSweeper.vue
@@ -5,8 +5,11 @@
 <template>
 	<div>
 		Mine Sweeper
-		<div v-for="y in 10" :key="y">
-			<button w-10 h-10 border="~" border-color="gray" v-for="x in 10" :key="x">{{ (y - 1) * 10 + x }}</button>
+		<div w-full overflow-auto>
+			<div v-for="y in 10" :key="y" flex="~" items-center justify-center w-max ma>
+				<button min-w-10 min-h-10 border="~" border-color="gray" v-for="x in 10" :key="x">{{ (y - 1) * 10 + x
+				}}</button>
+			</div>
 		</div>
 	</div>
 </template>
