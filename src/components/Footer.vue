660adab638b73b53654e7449fb801d3741281098
2389451262@qq.com <2389451262@qq.com>
2022-05-21 22:36:32 +0800

    chore:add tips

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,9 @@ import { toggleDark } from '../composables/dark'
 
 </script>
 <template>
+	<div mt-5>
+		开发进度:20%
+	</div>
 	<nav mt-6>
 		<a class="icon-btn" href="https://github.com/Huangyuanx/Vue3-MineSweeper" target="_blank" title="GitHub">
 			<div i="carbon-logo-github"></div>
